f6f99ebde5251a7ec48ca9e0491d1a002f0e4ff4
meshalalkhozai <meshalalkhozai@gmail.com>
2024-04-21 19:19:44 +0300

    Initial Commit

---
+++ wheatherLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wheatherLab
+//
+//  Created by meshal alkhozaei on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wheatherLab/wheatherLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheatherLabApp.swift
+//  wheatherLab
+//
+//  Created by meshal alkhozaei on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct wheatherLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
